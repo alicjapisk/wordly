911bb1244e2d8e815421c6cee674b7c54fec5891
alicjapisk <alicjapisk@gmail.com>
2025-08-07 23:42:58 +0200

    added session

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,15 @@
 import HomeScreen from "@/components/home/HomeScreen";
+import useSession from "@/hooks/useSession";
+import { Redirect } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 
 export default function App() {
+  const { session, isLoading } = useSession();
+
+  if (!isLoading && session) {
+    return <Redirect href="/user-courses" />;
+  }
   return (
     <>
       <StatusBar style="auto" />
